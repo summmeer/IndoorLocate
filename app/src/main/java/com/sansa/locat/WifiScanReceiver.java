0fd2d22dfc9fae414ccc74b08a9a96e71c55d643
summmeer <gongshansan@sjtu.edu.cn>
2018-04-25 09:15:40 +0800

    basic function to detect wifi signal

---
+++ app/src/main/java/com/sansa/locat/WifiScanReceiver.java
@@ -0,0 +1,21 @@
+package com.sansa.locat;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+import java.util.List;
+
+/**
+ * Created by sansa on 2018/4/13.
+ */
+
+public class WifiScanReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context c, Intent intent) {
+
+    }
+}
